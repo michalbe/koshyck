ffb8c917b92ec13d6f69089e8d66f0373985acfe
Michał Budzyński <michal@virtualdesign.pl>
2015-02-20 16:46:27 +0100

    :spaghetti: kind of working copy

--- app/product/product.js
+++ app/product/product.js
@@ -2,7 +2,7 @@ steal(
 'can',
 './product.mustache!',
 function(can, prTemplate) {
-  
+
   var Products = can.Model.extend({
     parseModels: function(response) {
       response = response.map(function(element) {
@@ -50,11 +50,6 @@ function(can, prTemplate) {
       },
       showSports: function() {
         this.filter('category', 'Sports Accessories');
-      }
-    },
-    events: {
-      'inserted' : function(){
-        console.log('inserted');
       }
     }
   });
